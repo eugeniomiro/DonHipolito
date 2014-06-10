979f56c7c94bf1846c0468622c106aad34ecac48
Eugenio Miró <eugenio.miro@gmail.com>
2014-06-10 15:52:41 -0300

    daily Backup update

--- dailyBackup.ps1
+++ dailyBackup.ps1
@@ -1,2 +1,9 @@
-﻿backup1.ps1 -dbname marketing
-backup1.ps1 -dbname tecnologia
+﻿$logfilename = "backup.log"
+
+backup1.ps1 -dbname tecnologia *>&1 | Out-File -Append $logfilename
+backup1.ps1 -dbname marketing  *>&1 | Out-File -Append $logfilename
+#backup1.ps1 -dbname ussgps     *>&1 | Out-File -Append $logfilename
+#backup1.ps1 -dbname openuss    *>&1 | Out-File -Append $logfilename
+
+# enviar logfilename por email
+# remove-item $logfilename
